250d9b41866ab3495eb97cf44cccc7b2c96946ef
BaoNguyen <17146084@student.hcmute.edu.vn>
2021-05-24 22:58:27 +0700

    update 24.5 v2

--- nhandiencd.py
+++ nhandiencd.py
@@ -9,100 +9,77 @@ from sklearn import metrics
 import numpy as np
 import pickle
 import cv2
-
+import math as m
 import matplotlib.pyplot as plt
 
 def motion_kinds(point_detect):
-        x = []
-        y = []
-        for i in point_detect:
-            x.append(i[0])
-            y.append(i[1])
         point_detect = np.array(point_detect)
-        
-        detect = np.arange(15).reshape((15,1)).tolist()
-
+        detect = np.arange(len(point_detect)).reshape((len(point_detect),1)).tolist()
         i_point_detect = point_detect[0]
-        for iii in range(0,15):
-            detect[int(iii)] =  [point_detect[int(iii)][0] - i_point_detect[0],
-                            point_detect[int(iii)][1] - i_point_detect[1],
-                            point_detect[int(iii)][2] - i_point_detect[2]]
+        for i in range(len(point_detect)):
+            P15_distance[i] = m.sqrt(m.pow(point_detect[i][0]-i_point_detect[i][0])
+                                    +m.pow(point_detect[i][1]-i_point_detect[i][1])
+                                    +m.pow(point_detect[i][2]-i_point_detect[i][2]))
+        
+        for i in range(0,15):
+            detect[int(i)] =  [point_detect[int(i)][0] - i_point_detect[0],
+                                point_detect[int(i)][1] - i_point_detect[1],
+                                point_detect[int(i)][2] - i_point_detect[2]]
         detect=np.array(detect)
         detect = np.reshape(detect,(1,45))
-
         a = loaded_model.predict(detect)
-
-        plt.plot(x, y, color = 'black' , linewidth=2,
-            label="dongtac %d" %a)
-        plt.show()
-
         return a
 
 def motion_detection(point_3d):
-    global first_loop, cnt, fifteen_temporary_points, three_temporary_points
+    global first_loop, cnt, Points_15, Points_3
     if first_loop == True:
-        #  original 15 point3d
-        fifteen_temporary_points[cnt - 1] = [point_3d[0],
+        Points_15[cnt - 1] = [point_3d[0],
                             point_3d[1],
                             point_3d[2]]
         cnt+=1
         if cnt >= 15:
             first_loop = False
             cnt = 0
-            return motion_kinds(fifteen_temporary_points)
+            return motion_kinds(Points_15)
     else:
-       
         if cnt <= 2:
-                          
-            three_temporary_points[cnt] = [point_3d[0],
+            Points_3[cnt] = [point_3d[0],
                                 point_3d[1],
                                 point_3d[2]]
             cnt+=1
-
         else:
             for ii in range(0,12):
-                fifteen_temporary_points[int(ii)] = [fifteen_temporary_points[int(ii)+3][0],
-                                                    fifteen_temporary_points[int(ii)+3][1],
-                                                    fifteen_temporary_points[int(ii)+3][2]]
-                
+                Points_15[int(ii)] = [Points_15[int(ii)+3][0],
+                                                    Points_15[int(ii)+3][1],
+                                                    Points_15[int(ii)+3][2]]
             for ii in range(12,15):
-                fifteen_temporary_points[int(ii)] = [three_temporary_points[int(ii)-12][0],
-                                    three_temporary_points[int(ii)-12][1],
-                                    three_temporary_points[int(ii)-12][2]]
+                Points_15[int(ii)] = [Points_3[int(ii)-12][0],
+                                    Points_3[int(ii)-12][1],
+                                    Points_3[int(ii)-12][2]]
             cnt = 0
-            
-        return motion_kinds(fifteen_temporary_points)
+        return motion_kinds(Points_15)
 
- 
 if __name__ == "__main__":
     try:
         cap1 = cv2.VideoCapture('/home/bao/Downloads/outpy.avi')
-        print('aaa')
-
         f1 = np.load("/home/bao/Downloads/save3d.npy")
-        print('bbbb')
         loaded_model = pickle.load(open('/home/bao/Desktop/DoAnTotNghiep/sp/traindongtac.sav', 'rb'))
-
+        #initialized variable
         first_loop = True
-        three_temporary_points = np.arange(3).reshape((3,1)).tolist()
-        fifteen_temporary_points = np.arange(15).reshape((15,1)).tolist()
+        Points_3 = np.arange(3).reshape((3,1)).tolist()
+        Points_15 = np.arange(15).reshape((15,1)).tolist()
+        P15_distance = np.arange(15).reshape((15,1)).tolist()
         cnt = 0
-
+        #run video
+        d = m.sqrt(m.pow()+m.pow()+m.pow())
         for index in range(int(cap1.get(7))):
-
             ret,frame = cap1.read()
             point_3d = f1[int(index)]
             if point_3d is not None:
-                if first_loop == True:
-                    k = motion_detection(point_3d)
-                else:
-                    if abs(point_3d[0]-fifteen_temporary_points[14][0]) <= 0.05 and abs(point_3d[1]-fifteen_temporary_points[14][1])<=0.05 and abs(point_3d[2]-fifteen_temporary_points[14][2])<=0.05:
-                        continue
-                    else:
-                        k = motion_detection(point_3d)
-                # print(k)
+                
+                    pass
+            #show video
             cv2.imshow('video',frame)
-
             if cv2.waitKey(30) ==27 :
                 break
     except Exception as ex:
--- test.py
+++ test.py
@@ -1,46 +1,19 @@
+#!/usr/bin/env python
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+from sklearn import svm
+from sklearn import metrics
+
 import numpy as np
+import pickle
+import cv2
+import math as m
 import matplotlib.pyplot as plt
 
-from sklearn.linear_model import Ridge
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline
 
 
-def f(x):
-    """ function to approximate by polynomial interpolation"""
-    return x * np.sin(x)
-
-
-# generate points used to plot
-x_plot = np.linspace(0, 10, 100)
-
-x = np.linspace(0, 10, 100)
-
-rng = np.random.RandomState(0)
-rng.shuffle(x)
-x = np.sort(x[:20])
-
-y = f(x)
-# create matrix versions of these arrays
-X = x[:, np.newaxis]
-X_plot = x_plot[:, np.newaxis]
-
-
-colors = ['teal', 'yellowgreen', 'gold']
-lw = 2
-plt.plot(x_plot, f(x_plot), color='cornflowerblue', linewidth=lw,
-         label="ground truth")
-plt.scatter(x, y, color='navy', s=30, marker='o', label="training points")
-# print(x)
-# print("y 2",y)
-for count, degree in enumerate([3, 4, 5]):
-    model = make_pipeline(PolynomialFeatures(degree), Ridge())
-    
-    model.fit(X, y)
-    y_plot = model.predict(X_plot)
-    plt.plot(x_plot, y_plot, color=colors[count], linewidth=lw,label="degree %d" %degree)
-    print(x_plot.shape)
-    # print(y_plot)
-plt.legend(loc='lower left')
-
-# plt.show()
+a = np.arange(15).reshape((15,)).tolist()
+print(a)
+print(np.count_nonzero(a>5))
